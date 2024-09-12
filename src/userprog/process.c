efa047a56d41bd211e8acc35a1ffa35b7edfd590
Light-creator <ibadulasha@ya.ru>
2024-09-12 12:10:55 +0300

    bugfix lock_release in process_execute

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -39,7 +39,6 @@ process_execute (const char *file_name)
   for(; file_name[i] != ' ' && i < strlen(file_name); i++)
     new_filename[i] = file_name[i];
   new_filename[i] = '\0';
-  //printf("new_filename: [%s]\n", new_filename);
 
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
@@ -48,9 +47,7 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
   sema_init(&sema_proc, 0);
-  // printf("process_execute(%s)\n", file_name);
 
-  // printf("process_execute(%s)\n", file_name);
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (new_filename, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
@@ -61,18 +58,15 @@ process_execute (const char *file_name)
 struct thread* get_child(tid_t pid) {
   struct list* lst = &thread_current()->children;
   if(!lst || list_empty(lst)) return NULL;
-  // printf("lst: %p\n", lst);
 
   struct list_elem *e;
 
   for (e = list_begin(lst); e != list_end(lst); e = list_next(e)) {
       struct thread *t = list_entry(e, struct thread, child_elem);
       if(!t || t->tid < 0) return NULL;
-      // printf("t: %p | %s | %d\n", t, t->name, t->tid);
       if(t->tid == pid) return t;
   }
 
-  // printf("return NULL\n");
   return NULL;
 }
 
@@ -144,15 +138,11 @@ start_process (void *file_name_)
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
 
-  // printf("start_process(%s)\n", new_filename);
   success = load(new_filename, &if_.eip, &if_.esp);
-  // printf("load(%s): %d\n", new_filename, success);
 
-  // printf("Before construct_esp\n");
   if(success)
     construct_esp(file_name, &if_.esp);
 
-  // printf("After construct_esp\n");
   struct thread* curr = thread_current();
   curr->load_status = success;
   sema_up(&curr->load_sema);
@@ -190,18 +180,11 @@ process_wait (tid_t child_tid UNUSED)
 
   int res_status = -1;
 
-  // if(child_process->is_exit) {
-  //   res_status = child_process->exit_status;
-  //   goto done;
-  // }
-  // printf("sema_up(wait_sema)\n");
   sema_up(&child_process->wait_sema);
-  // printf("sema_down(exit_sema)\n");
-  res_status = child_process->exit_status;
-  sema_down(&child_process->exit_sema); 
-  // printf("exit_status: %d\n", res_status);
 
-// done:
+  res_status = child_process->exit_status;
+  sema_down(&child_process->exit_sema);
+
   child_process->child_elem.next = child_process->child_elem.next;
   child_process->child_elem.prev = child_process->child_elem.prev;
   return res_status;
@@ -212,16 +195,14 @@ void
 process_exit (void)
 {
   struct thread *cur = thread_current ();
-  // printf("Before sema_down(wait_sema)\n");
-  sema_down(&cur->wait_sema);
-  // printf("After sema_down(wait_sema)\n");
 
-  // printf("process_exit(%s) tid = %d\n", cur->name, cur->tid);
+  sema_down(&cur->wait_sema);
+
   uint32_t *pd;
   
   cur->is_exit = true;
   printf("%s: exit(%d)\n", cur->name, cur->exit_status);
-  // printf("sema_up(exit_sema)\n");
+
   sema_up(&cur->exit_sema);
   /* Destroy the current process's page directory and switch back
      to the kernel-only page directory. */
@@ -234,10 +215,6 @@ process_exit (void)
          directory before destroying the process's page
          directory, or our active page directory will be one
          that's been freed (and cleared). */
-    // cur->is_exit = true;
-    // sema_up(&cur->exit_sema);
-    // printf("%s: exit(%d)\n", cur->name, cur->exit_status);
-    // sema_up(&sema_proc);
     cur->pagedir = NULL;
     pagedir_activate (NULL);
     pagedir_destroy (pd);
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,6 +6,7 @@
 #include "userprog/process.h"
 #include "threads/vaddr.h"
 #include "userprog/pagedir.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -16,8 +17,10 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static bool is_valid_ptr(void* ptr);
+static void is_valid_ptr(void* ptr);
 static void invalid_exit(int exit_status);
+
+static void sys_halt();
 
 // Processes methods
 static int sys_wait(int pid);
@@ -36,27 +39,17 @@ static int sys_write(int fd, const void* buffer, size_t size);
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-
-  // printf("f->esp = %p\n", f->esp);
   int* ptr = f->esp;
-  bool valid = true; 
-  for(int i=0; i<4; i++) valid &= is_valid_ptr(ptr+i);
-
-  if(!valid) { 
-    f->eax = -1;
-    return;
-  }
+  for(int i=0; i<4; i++) is_valid_ptr(ptr+i);
 
   switch(*(int*)f->esp) {
     case SYS_EXIT:
       sys_exit(*(ptr+1));
       break;
     case SYS_WAIT:
-      // printf("sys_wait(%d)\n", ((size_t *)f->esp)[1]);
       f->eax = sys_wait(*(ptr+1));
       break;
     case SYS_EXEC:
-      // is_valid_ptr(ptr+1);
       f->eax = sys_exec(((const char **)ptr)[1]);
       break;
     case SYS_CREATE:
@@ -80,68 +73,48 @@ syscall_handler (struct intr_frame *f UNUSED)
     case SYS_WRITE:
       f->eax = sys_write(*(ptr+1), ((void**) ptr)[2], *(ptr+3));
       break;
+    case SYS_HALT:
+      sys_halt();
+      break;
   }
 
 }
 
-/*
-Tests:
-- exec-bad-ptr
-- open-bad-ptr
-- create-bad-ptr
-- wait-bad-ptr
-- close-bad-ptr
-- read-bad-ptr
-- write-bad-ptr
-- wait-killed
-*/
-static bool is_valid_ptr(void* ptr) {
-  // printf("ptr %#010x\n", *(uint32_t*)ptr);
-  // printf("ptr = %p\n", ptr);
-  if(ptr == 0x20101234 || ptr == 0xc0100000) {
+static void is_valid_ptr(void* ptr) {
+  if(ptr >= PHYS_BASE) {
     sys_exit(-1);
     sema_up(&thread_current()->wait_sema);
   }
 
-  if (ptr >= PHYS_BASE ||
-    ptr == 0x20101234 ||
-    *(uint32_t*)ptr == 0x20101234 ||  
-    *(uint32_t*)ptr == 0xc0100000 ||  
+  if (*(uint32_t*)ptr >= PHYS_BASE ||
     pagedir_get_page(thread_current()->pagedir, (const void *)ptr) == NULL) {
-      // printf("invalid ptr %#010x\n", *(uint32_t*)ptr);
       sys_exit(-1);
       sema_up(&thread_current()->wait_sema);
   }
-
-  return true;
 }
 
 void sys_exit(int exit_status) {
-  // printf("sys_exit(%d)\n", exit_status);
   thread_current()->exit_status = exit_status;
   thread_exit();
 }
 
 static int sys_wait(int pid) {
-  // printf("sys_wait(%d)\n", pid);
   return process_wait(pid);
 }
 
 static int sys_exec(const char* cmd_line) {
   lock_acquire(&file_lock);
   tid_t pid = process_execute(cmd_line);
-  // printf("pid: %d\n", pid);
-  // printf("process_execute pid: %d\n", pid);
+  lock_release(&file_lock);
+
   if(pid == TID_ERROR) return -1;
 
   struct thread* child_process = get_child(pid);
-  if(!child_process) return -1;
-  // printf("child_process->name = %s\n", child_process->name);
+  if(!child_process)  return -1;
+
   if(!child_process->load_status) sema_down(&child_process->load_sema);
 
-  // printf("child_process-load_status = %d\n", child_process->load_status);
   if(!child_process->load_status) return -1;
-  lock_release(&file_lock);
 
   return pid; 
 }
@@ -217,6 +190,10 @@ static int sys_write(int fd, const void* buffer, size_t size) {
 
   lock_release(&file_lock);
   return res_status;
+}
+
+static void sys_halt() {
+  shutdown_power_off();
 }
 
 static void invalid_exit(int exit_status) {
